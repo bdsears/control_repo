930c7020df72bc44b2dd57a0f09800f22365f215
Brian Sears <34166782+bdsears@users.noreply.github.com>
2019-09-20 10:05:10 -0400

    added ssh key to ssh_server.  Added ssh role to web and db servers

--- site/role/manifests/ssh_server.pp
+++ site/role/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSEoUuwsCIRQwBZNtTw0KyI/QJLSwv6xPQc7ZE1wsodWtY8zfy6g0db1U2xutghpit+njagEaGNnz8cY+g3e3upzCjst3ywfGOg2SN0sbBnsTWd23HLZXqNBfDGZavsj6FBhltTaw3N+DZl9kwYrXxFW1KpuwhjO5FLgbNCGF3ZTOwpq7/K5v8LCig3YFhUnzyxiHnwEoF/6xnZDNFm+XcrTavkZn5DubzGTJ2BEiz+TUP0FGfOY0aQvsHZFQw717CBS87GepQYp/E3wrubciVtDz9IBbIAwOyBloyfj/PNc6jkfg0nNeKsx9a94vE2cmO3TGAghG+RiHHfHyscMRv root@master.puppet.vm',
 	}  
 }

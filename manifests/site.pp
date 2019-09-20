0314f74b75281c03bb11a6ae096d7133ee86925f
Brian Sears <34166782+bdsears@users.noreply.github.com>
2019-09-20 10:10:28 -0400

    added missing commas to ends of lines

--- manifests/site.pp
+++ manifests/site.pp
@@ -10,12 +10,12 @@ node 'master.puppet.vm' {
 }
 
 node /^web/ {
-  include role::app_server
-  include role::ssh_server
+  include role::app_server,
+  include role::ssh_server,
 }
 
 node /^db/ {
-  include role::db_server
-  include role::ssh_server
+  include role::db_server,
+  include role::ssh_server,
 }
 
